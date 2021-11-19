e9dd1f7c79483a02216a8aca2d6a6f4b2c8d1a2a
Nicole Gray <ngray122@gmail.com>
2021-11-18 22:59:01 -0500

    Hoster Clone. HTML/CSS/JS

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function hiringNow() {
+    alert("Hang tight! Redirecting to our career platform!!")
+}
+
+function joinList() {
+    alert("We'll be in touch soon, thanks for joining!")
+
+}
+
+ function unsubscribeLink() {
+     alert("Are you sure? So sad to see you go! 😢  To confirm, press ok")
+ }
